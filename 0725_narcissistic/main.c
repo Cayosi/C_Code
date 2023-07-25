ab4806c60f614892db8825b7d6c3c680b9cbe81e
Cayosi <shaopengli144@gmail.com>
2023-07-25 14:53:14 +0800

    narcissistic number

---
+++ 0725_narcissistic/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int test = 994;
+    int a, b, c;
+    for (int i = 100; i < 1000; i++) {
+        a = i % 10;
+        b = i / 10 % 10;
+        c = i / 100;
+        if (a * a * a + b * b * b + c * c * c == i) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
